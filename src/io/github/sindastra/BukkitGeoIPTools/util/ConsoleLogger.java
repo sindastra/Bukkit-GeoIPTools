b0e6737b1f720741a44aaeefe7cdc9676e8bcd38
Sindastra <1236984+sindastra@users.noreply.github.com>
2018-04-18 18:47:09 +0200

    Fixed previous author's name (special char) and added own copyright.

--- src/io/github/sindastra/BukkitGeoIPTools/util/ConsoleLogger.java
+++ src/io/github/sindastra/BukkitGeoIPTools/util/ConsoleLogger.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2011 Sebastian K�hler <sebkoehler@whoami.org.uk>.
+ * Copyright 2011 Sebastian Köhler <sebkoehler@whoami.org.uk>.
+ * Copyright 2018 Sindastra <https://github.com/sindastra/Bukkit-GeoIPTools>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
